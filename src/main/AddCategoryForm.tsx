b394dbc9fe79def91b0ce81ace71523ea357303d
Ayşegül Kavaklı <aysegulkavakli@Imac-iMac-2.local>
2022-03-02 10:47:58 +0300

    Add Status implemented

--- src/main/AddCategoryForm.tsx
+++ src/main/AddCategoryForm.tsx
@@ -5,22 +5,24 @@ import CustomDrawer from "./CustomDrawer";
 import AddStatusForm from "./AddStatusForm";
 
 interface Category {
-    title:string;
+    title: string;
 }
-export default function AddCategoryForm(){
-    const [category, setCategory] = useState<Category>({title:""});
-    const [categories,setCategories] =useState<any>();
-    const [open, setOpen] = React.useState(false);
 
-    const handleChange  = (e:React.ChangeEvent<HTMLInputElement>) => {
+export default function AddCategoryForm() {
+    const [category, setCategory] = useState<Category>({title: ""});
+    const [categories, setCategories] = useState<any>();
+    const [open, setOpen] = React.useState({isVisible:false, category:null});
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newData = {...category}
         newData[e.currentTarget.name as keyof Category] = e.currentTarget.value
         setCategory(newData)
     }
-    function addCategory(){
+
+    function addCategory() {
         AxiosInstance.post('/category',
             category,
-            )
+        )
             .then(function (response) {
                 console.log(response)
             })
@@ -28,36 +30,37 @@ export default function AddCategoryForm(){
                 console.log(error);
             });
     }
-    function getCategory(){
-        AxiosInstance.get('/category',
 
+    function getCategory() {
+        AxiosInstance.get('/category',
         )
             .then(function (response) {
-                setCategories(response.data.map((d:any)=>d.title))
+                setCategories(response.data)
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
-    useEffect(()=>{
+
+    useEffect(() => {
         getCategory();
-    },[])
+    }, [])
 
 
-    return(
+    return (
         <Stack direction="column" p={3} spacing={2}>
-            <Input  placeholder="Category name" name="title" value={category.title} onChange={handleChange}/>
-            <Button variant="contained"  onClick={()=>addCategory()} >Add Category</Button>
-            {categories && categories.map((c:string)=> (
+            <Input placeholder="Category name" name="title" value={category.title} onChange={handleChange}/>
+            <Button variant="contained" onClick={() => addCategory()}>Add Category</Button>
+            {categories && categories.map((c: any) => (
                 <Stack direction="row" display="flex" alignItems="center" spacing={3}>
                     <Box>
-                        {c}
+                        {c.title}
                     </Box>
-                   <Button onClick={()=>setOpen(true)}>Statüleri Düzenle</Button>
+                    <Button onClick={() => setOpen({isVisible:true, category: c})}>Statüleri Düzenle</Button>
                 </Stack>
-                ))
+            ))
             }
-            <CustomDrawer open={open} setOpen={setOpen}  children={<AddStatusForm />}/>
+            <CustomDrawer open={open.isVisible} setOpen={()=>setOpen({isVisible:false, category:null})} children={<AddStatusForm category={open.category}/>}/>
         </Stack>
     )
 }
--- src/main/AddStatusForm.tsx
+++ src/main/AddStatusForm.tsx
@@ -3,24 +3,27 @@ import {AxiosInstance} from "./AxiosClient";
 import {Box, Button, Input, Stack} from "@mui/material";
 
 interface Status {
-    title:string;
-    categoryId:string;
-    color:string;
+    title: string;
+    categoryId: string;
+    color: string;
 }
 
-interface Props{
-    category?:{title:string, id:string}
+interface Props {
+    category?: any;
 }
-export default function AddStatusForm({category}:Props){
-    const [status, setStatus] = useState<Status>({title:"", categoryId:"",color:""});
-    const [statusTitles,setStatusTitles] =useState<any>();
 
-    const handleChange  = (e:React.ChangeEvent<HTMLInputElement>) => {
+export default function AddStatusForm({category}: Props) {
+    const [status, setStatus] = useState<Status>({title: "", categoryId: category.id, color: ""});
+    const [statusTitles, setStatusTitles] = useState<any>();
+    const categoryId = category.id;
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newData = {...status}
         newData[e.currentTarget.name as keyof Status] = e.currentTarget.value
         setStatus(newData)
     }
-    function addStatus(){
+
+    function addStatus() {
         AxiosInstance.post('/status',
             status,
         )
@@ -31,33 +34,36 @@ export default function AddStatusForm({category}:Props){
                 console.log(error);
             });
     }
-    function getStatus(){
-        AxiosInstance.get('/status',
 
-        )
+    function getStatusList() {
+        AxiosInstance.get('/status',
+            { params: { categoryId: categoryId } })
             .then(function (response) {
-                setStatusTitles(response.data.map((d:any)=>d.title))
+                console.log("response", response)
+                setStatusTitles(response.data)
+
             })
             .catch(function (error) {
                 console.log(error);
             });
     }
-    useEffect(()=>{
-        getStatus();
-    },[])
 
-    return(
+    useEffect(() => {
+        category && getStatusList();
+    }, [])
+
+    return (
         <Stack direction="column" p={3} spacing={2}>
-            <Input  placeholder="Status name" name="title" value={status.title} onChange={handleChange}/>
-            <Input  placeholder="Status color" name="color" value={status.color} onChange={handleChange}/>
-            <Button variant="contained"  onClick={()=>addStatus()} >Add Category</Button>
-            {statusTitles && statusTitles((s:string)=> (
+            <Input placeholder="Status name" name="title" value={status.title} onChange={handleChange}/>
+            <Input placeholder="Status color" name="color" value={status.color} onChange={handleChange}/>
+            <Button variant="contained" onClick={() => addStatus()}>Add Category</Button>
+            {statusTitles && statusTitles((s: any) => (
                 <Stack direction="row" display="flex" alignItems="center" spacing={3}>
-                    <Box >
-                        {s}
+                    <Box>
+                        {s.title}
                     </Box>
                     <Button>Sil</Button>
-                    <Button> Düzenle</Button>
+                    <Button>Düzenle</Button>
                 </Stack>
             ))
             }
